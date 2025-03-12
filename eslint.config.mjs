c38400c6212440ceebeeddc8ac41affe66c6860e
Bill Bunkum <billbunkum@Billy-Boy-Uno.lan>
2025-03-12 02:45:00 -0700

    config hell; disable lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,10 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+	rules: {
+		'ract/no-unescaped-entities': 'off',
+		'@next/next/no-page-custom-font': 'off',
+	},
 ];
 
 export default eslintConfig;
