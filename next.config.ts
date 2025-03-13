1fa12c9e5151c7fd51eeaa2a11f13205fbc22278
Bill Bunkum <billbunkum@Billy-Boy-Uno.lan>
2025-03-13 04:31:42 -0700

    edit next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+/*import path from 'path'
+@mport getConfig from 'next/config'
+*/
 
 const nextConfig: NextConfig = {
   /* config options here */
+	serverRuntimeConfig: {
+		PROJECT_ROOT: __dirname
+	}
+
 };
+
+
 
 export default nextConfig;
